b969446be6b78acfe5fa05ce8d41942c999d3431
Nisha Chaudhary <nishachaudhary91101@gmail.com>
2026-04-14 12:12:47 +0530

    practice of more datatypes

---
+++ 01_basics/boolean.py
@@ -0,0 +1,6 @@
+my_str ="Nisha Chaudhary"
+print(my_str.isalnum())
+print(my_str.isalpha())
+print(my_str.islower())
+print(my_str.endswith("ary"))
+print(my_str.startswith("N"))
--- 01_basics/list.py
+++ 01_basics/list.py
@@ -1,6 +1,25 @@
-# list is mutable,ordered,allow duplicate values, heterogenous
+# list is a collection of items that is mutable,ordered,allow duplicate values, heterogenous
 
 fruits = ["apple", "banana", "guava"]
 
 a = [12,13,23,34,45,True,34.6]
-print(a[0,5])
+print(len(a))
+print(a[0],a[5])
+
+# list methods->
+a = [1,2,3,4,5,6]
+a.append(7)
+print(a)
+a.insert(2,8)
+print(a)
+a.extend([9,10])
+print(a)
+a.remove(2)
+print(a)
+a.reverse()
+print(a)
+a.pop(0)
+print(a)
+a.count(3)  # total occurences of the element
+print(a)
+
