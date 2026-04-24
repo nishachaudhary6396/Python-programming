fa18e210548a182cb7a271c0240b874f2b57954a
Nisha Chaudhary <nishachaudhary91101@gmail.com>
2026-04-24 14:38:57 +0530

    Hospital Management by using pillars of Oops

--- Oops_basics/Encapsulation/PrivateProperties.py
+++ Oops_basics/Encapsulation/PrivateProperties.py
@@ -2,22 +2,22 @@
 
 # #Traditional way->
 # #get method
-# class human:
-#     def __init__(self,name,clg):
-#         self.name = name
-#         self.__clg = clg
+class human:
+    def __init__(self,name,clg):
+        self.name = name
+        self.__clg = clg
 
-#     def get_clg(self):    #  to get the private value
-#         return self.__clg
+    def get_clg(self):    #  to get the private value
+        return self.__clg
     
-#     def set_clg(self,clg):  #set method-> to changet the private property
-#         self.__clg = clg
+    def set_clg(self,clg):  #set method-> to changet the private property
+        self.__clg = clg
 
-# h1 = human("Nisha","gla")
-# print(h1.name)
-# print(h1.get_clg())
-# h1.set_clg("gla University")
-# print(h1.get_clg())
+h1 = human("Nisha","gla")
+print(h1.name)
+print(h1.get_clg())
+h1.set_clg("gla University")
+print(h1.get_clg())
 
 #pythonic way->
 class BankAccount:
---
+++ Oops_basics/HospitalManagement.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod 
+class hospital(ABC):
+    @abstractmethod
+    def get_role(self):
+        pass
+class person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def display_details(self):
+        print(self.name,self.age)
+class doctor(person,hospital):
+    def __init__(self,name,age,attributes):
+        super().__init__(name,age)
+        self.attributes = attributes
+    def display_details(self):
+        print(f"Name: {self.name},Age: {self.age},Specialization: {self.attributes}")
+
+    def get_role(self):
+        print("I am doctor")
+class patient(person,hospital):
+    def __init__(self,name,age,disease):
+        super().__init__(name,age)
+        self.__disease = disease
+    def display_details(self):
+        print(f"name:{self.name},Age:{self.age}, Disease:{self.__disease}")
+
+    def get_role(self):
+        print("I m pateint")
+    
+    def get_disease(self):   #getter
+        return self._disease
+    
+    def set_disease(self,new_disease):
+        self._disease = new_disease
+        
+
+dc = doctor("Nisha","22","Cardiologist")
+p = patient("Riya","21","Fever")
+dc.display_details()
+p.display_details()
+dc.get_role()
+p.get_role()
+
+
+
+
+
