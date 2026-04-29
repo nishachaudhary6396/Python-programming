79fb8100569dc79ee0b81a32b5208f9d8035e246
Nisha Chaudhary <nishachaudhary91101@gmail.com>
2026-04-29 13:36:40 +0530

    registration and login problem with oops concept and json file handling

--- regex/PracticeProblem.py
+++ regex/PracticeProblem.py
@@ -1,39 +1,81 @@
-# Student Login and Registration Regex Practice Problem
-
 import re
-#Registeration
-name = input("Enter name: ")
-address = input("Enter address: ")
-student_id = input("Enter student Id: ")
-password = input("Enter password: ")
+import json
+import os
 
-name_pattern = r"^[a-zA-Z ]{3,30}$"
-address_pattern = r"^[a-zA-Z ,.-/]{10,100}$"
-studentid_pattern = r"^STU\d{4}$"
-password_pattern = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$"
+class StudentAuth:
 
-if not re.match(name_pattern,name):
-    print("Invalid name")
-elif not re.match(address_pattern,address):
-    print("INvalid address")
-elif not re.match(studentid_pattern,student_id):
-    print("Invalid student_id")
-elif not re.match(password_pattern,password):
-    print("Invalid Passowrd")
-else:
-    print("Registeration Successful!!!!!")
-#login
-    login_id = input("Enter login Id")
-    login_psw = input("Enter login password")
-    if login_id==student_id and login_psw==password:
-        print("login Successful")
-    else:
-        print("Invalid student Id or password")
+    FILE = "students.json"
+
+    #  Validation methods
+    def validate_name(self, name):
+        pattern = r"^[a-zA-Z ]{3,30}$"
+        return re.match(pattern, name)
+
+    def validate_address(self, address):
+        pattern = r"^[a-zA-Z0-9 ,.-/]{10,100}$"
+        return re.match(pattern, address)
+
+    def validate_student_id(self, student_id):
+        pattern = r"^STU\d{4}$"
+        return re.match(pattern, student_id)
+
+    def validate_password(self, password):
+        pattern = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$"
+        return re.match(pattern, password)
+    
+    def load_data(self):     #to load the data
+        if os.path.exists(self.FILE):
+            with open(self.FILE, "r") as f:
+                return json.load(f)
+        return {}
+    
+    def save_data(self, data):   #to dump the data
+        with open(self.FILE, "w") as f:
+            json.dump(data, f)
 
 
+    #  Registration
+    def register(self):
+        name = input("Enter name: ")
+        if not self.validate_name(name):
+            print("Invalid name")
+            return
+        
+        address = input("Enter address: ")
+        if not self.validate_address(address):
+            print("Invalid address")
+            return
 
+        student_id = input("Enter student Id: ")
+        if not self.validate_student_id(student_id):
+            print("Invalid student ID")
+            return
 
+        password = input("Enter password: ")
+        if not self.validate_password(password):
+            print("Invalid password")
+            return
+        
+        data = self.load_data()    #load existing data
 
+        data[student_id] = password    #save
+        self.save_data(data)
 
+        print("Registration Successful!!!!!")
 
+        self.login()
 
+    #  Login
+    def login(self):
+        login_id = input("Enter login Id: ")
+        login_psw = input("Enter login password: ")
+
+        data = self.load_data()
+
+        if login_id in data and data[login_id] == login_psw:
+            print("Login Successful")
+        else:
+            print("Invalid student Id or password")
+
+obj = StudentAuth()
+obj.register()
