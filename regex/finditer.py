17c557ab179c93391bd0562750da1f89d792b5ae
Nisha Chaudhary <134211249+nishachaudhary6396@users.noreply.github.com>
2026-05-18 16:13:17 +0530

    Delete regex directory

--- regex/finditer.py
+++
@@ -1,9 +0,0 @@
-import re
-all_games = "1920 Antwerp Olympics, 1924 Paris Olympics, 1928 Amsterdam Olympics"
-find_all_olympics = re.finditer("Olympics",all_games)
-for i in find_all_olympics:
-    match_txt = i.group()
-    start = i.start()
-    end = i.end()
-    span = i.span()
-    print(f"Match: {match_txt}, Start: {start}, End: {end}, Span: {span}")
