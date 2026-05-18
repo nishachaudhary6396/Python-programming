33f8bb4597f1a94e5e8158426e229f87fb28a539
Nisha Chaudhary <134211249+nishachaudhary6396@users.noreply.github.com>
2026-05-18 16:12:00 +0530

    Delete 01_basics directory

--- 01_basics/BasicCorePrograms.py/Q1.py
+++
@@ -1,7 +0,0 @@
-# Q1:- User Input and Replace String Template "Hello<<UserName>>, How are you?"
-
-name = input("Enter name :")
-if len(name)<3:
-    print("Name should have min 3 char")
-else:
-    print(f"Hello {name}, How are you?")
