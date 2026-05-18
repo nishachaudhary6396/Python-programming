ea91590e6e95b6c461bf7518ad76cc29ec0601c7
Nisha Chaudhary <134211249+nishachaudhary6396@users.noreply.github.com>
2026-05-18 15:53:55 +0530

    Delete pytest directory

--- pytest/Exceptionhandling/Test_Custom_Functions.py
+++
@@ -1,8 +0,0 @@
-import pytest
-def login(user):
-    if user != "admin":
-        raise ValueError("Invalid user")
-
-def test_login_error():
-    with pytest.raises(ValueError):
-        login("user")
