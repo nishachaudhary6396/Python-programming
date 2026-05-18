623eeb9fb81ff7b057ce0f04ad4bb4461d02c7e5
Nisha Chaudhary <134211249+nishachaudhary6396@users.noreply.github.com>
2026-05-18 16:03:17 +0530

    Delete regex directory

--- regex/UrlValidation.py
+++
@@ -1,8 +0,0 @@
-#https://google.com
-import re
-url = input("Enter URL: ")
-pattern = r"^https?://[a-zA-Z]\.[a-zA-Z]{2,}$"
-if re.match(pattern,url):
-    print("Valid Url")
-else:
-    print("Invalid URL")
--- regex/checkOnlyAlphabets.py
+++
@@ -1,7 +0,0 @@
-import re
-name = input("Enter name: ")
-pattern = r"^[a-zA-Z]+$"
-if re.match(pattern,name):
-    print("Valid name")
-else:
-    print("Invalid name")
